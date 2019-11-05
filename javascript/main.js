6042991da6f4426da819738ac2e444ea84d67b22
Ava <avatzeng@gmail.com>
2019-11-05 12:37:20 +0800

    加入首頁主標題與附標題字

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 $(function() {
     var s = skrollr.init({
         render: function(data) {
-            console.log(data.curTop);
+            // console.log(data.curTop);
         }
     });
 });
