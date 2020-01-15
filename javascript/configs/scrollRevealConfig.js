6311a20e8e7958b4dd374a63394b74302a9c999a
Ava <avatzeng@gmail.com>
2020-01-15 17:55:35 +0800

    修正首頁 header 動畫呈現

---
+++ javascript/configs/scrollRevealConfig.js
@@ -0,0 +1,12 @@
+const globalReset = false;
+const delayBase = 200;
+
+export default {
+    'CONTENT_UPWARD': {
+        delay: delayBase,
+        distance: '3rem',
+        origin: 'bottom',
+        reset: globalReset,
+        easing: 'cubic-bezier(0.23, 1, 0.32, 1)'
+    }
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,20 @@
+import { elements } from './views/base.js';
+import ScrollRevealConfig from './configs/scrollRevealConfig.js';
+import ScrollReveal from '../node_modules/scrollreveal';
+
 $(function() {
     var s = skrollr.init({
         render: function(data) {
-            console.log(data.curTop);
+            // console.log(data.curTop);
         }
     });
+
+    // setUpScrollRevealAnim();
+
 });
+
+
+// function setUpScrollRevealAnim() {
+//     // elements.titleDivider
+//     ScrollReveal().reveal(elements.titleDivider, ScrollRevealConfig.CONTENT_UPWARD);
+// }
